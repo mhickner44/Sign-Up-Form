64567876964df0e940d39559b51287b744443aca
mhickner44 <rmicu1903@gmail.com>
2023-02-16 09:11:38 -0500

    field validation 4/5 fields complete

---
+++ script.js
@@ -0,0 +1,10 @@
+const password = Document.getElementById("#password");
+const confirmPassword= Document.getElementById("#confirmPwd");
+
+function matchPasswords(){
+    if(password.textContent==confirmPassword.textContent){
+//validate the field and get rid of password messaeg
+
+    }
+
+}
