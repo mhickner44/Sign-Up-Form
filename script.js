62c236ee2f5e4eb033bb10248d86d58ca9895669
mhickner44 <rmicu1903@gmail.com>
2023-02-17 09:10:47 -0500

    form field validation complete

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const password = Document.getElementById("#password");
-const confirmPassword= Document.getElementById("#confirmPwd");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPwd");
+const errorMessage = document.querySelector(".errorMessage");
+const inputs = document.querySelectorAll("input[type=text]");
 
-function matchPasswords(){
-    if(password.textContent==confirmPassword.textContent){
-//validate the field and get rid of password messaeg
-
-    }
-
+function matchPasswords() {
+  if (password.value == confirmPassword.value) {
+    //validate the field and get rid of password messaeg
+    password.style.cssText = "border: 2px solid green; ";
+    confirmPassword.style.cssText = " border: 2px solid green; ";
+    errorMessage.textContent = "";
+  } else {
+    password.style.cssText =
+      "outline: none; box-shadow: 2px 2px 5px black; border: 2px solid red;";
+    confirmPassword.style.cssText =
+      "outline: none; box-shadow: 2px 2px 5px black; border: 2px solid red;";
+    errorMessage.textContent = "* Passwords do not match";
+  }
 }
+
+
+password.addEventListener("input", matchPasswords);
+confirmPassword.addEventListener("input", matchPasswords);
+
+inputs.forEach(function(elem) {
+    elem.addEventListener("input", function() {
+        elem.classList.add("postInput");
+    });
+});
